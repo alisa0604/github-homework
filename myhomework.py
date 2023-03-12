41c8fee2151b357c923b4294877c6b698d475688
alisa0604 <126905416+alisa0604@users.noreply.github.com>
2023-03-12 22:51:39 +0100

    Update myhomework.py

--- myhomework.py
+++ myhomework.py
@@ -1,32 +1,53 @@
-import re
-import os
+import sys
+from pathlib import Path
+
+CATEGORIES = {'audio': ['.mp3', '.aiff'],
+              'image': ['.png', '.jpg']}
+
+def get_categories(file:Path):
+    extension = file.suffix.lower()
+    for cat, exts in CATEGORIES.items():
+        if extension in exts:
+            return cat
+    return 'unknown'
+
+#path = Path(r'd:\testfolder')
+
+#for item in path.glob('**'):
+#    print(item)
+
+def move_file(file:Path, root_dir:Path, category:str):
+    if category == 'unknown':
+        return file.replace(root_dir.joinpath(file.name))
+
+    target_folder = root_dir.joinpath(category)
+
+    if not target_folder.exists():
+        target_folder.mkdir()
+    return file.replace(target_folder.joinpath(file.name))
 
 
-def normalize(filename):
-     invalid_chars = r'[^\w\d.]'
-     translit_dict = {
-        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e',
-        'ё': 'e', 'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k',
-        'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
-        'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'ts',
-        'ч': 'ch', 'ш': 'sh', 'щ': 'sch', 'ъ': '', 'ы': 'i', 'ь': '',
-        'э': 'e', 'ю': 'yu', 'я': 'ya'
-    }
-     filename = re.sub(invalid_chars, '_', filename)
-     for cyrillic, latin in translit_dict.items():
-        filename = filename.replace(cyrillic, latin)
+def sort_dir(root_dir:Path, current_dir):
+    for item in current_dir.glob('*'):
+        if not item.is_dir():
+            category = get_categories(item)
+            new_path = move_file(item, root_dir, category)
+            print(new_path)
+        else:
+            sort_dir(item)
+            item.rmdir()
+def main():
+    try:
+        path = Path(sys.argv[1])
+    except IndexError:
+        return f'No path to folder. Take as parametr'
+    
+    if not path.exists():
+        return "Sorry, folder not exist"
+    
+    sort_dir(path, path)
 
-     return filename
-
-import shutil
-
-
-def sort_files(directory):
-    image_dir = os.path.join(directory, 'images')
-    document_dir = os.path.join(directory, 'documents')
-    audio_dir = os.path.join(directory, 'audio')
-    video_dir = os.path.join(directory, 'video')
-    archive_dir = os.path.join(directory, 'archives')
-    for d in [image_dir, document_dir, audio_dir, video_dir, archive_dir]:
-        if not os.path.exists(d):
-            os.mkdir(d)
+    return "Everything is ok"
+    
+if __name__ == "__main__":
+    print(main())
