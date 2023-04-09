665cb15164d2e709c85c4326677f9f518223352f
alisa0604 <speedexciter6@gmail.com>
2023-04-09 22:00:42 +0200

    contactbook

---
+++ contactbook.py
@@ -0,0 +1,40 @@
+class Field:
+    def __init__(self, value=None):
+        self.value = value
+    
+    def __str__(self):
+        return str(self.value)
+    
+class Name(Field):
+    def __init__(self, value):
+        super().__init__(value)
+
+class Phone(Field):
+    def __init__(self, value=None):
+        super().__init__(value)
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                self.phones.remove(p)
+                break
+
+    def edit_phone(self, old_phone, new_phone):
+        for p in self.phones:
+            if p.value == old_phone:
+                p.value = new_phone
+                break
+
+from collections import UserDict
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
